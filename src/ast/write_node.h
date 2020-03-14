92e2a4493b339b248320f4cb2889e78935851a9a
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-14 19:51:01 +0000

    Rename print_node to write_node

--- src/ast/write_node.h
+++ src/ast/write_node.h
@@ -1,19 +1,19 @@
-#ifndef __OG_AST_PRINT_NODE_H__
-#define __OG_AST_PRINT_NODE_H__
+#ifndef __OG_AST_WRITE_NODE_H__
+#define __OG_AST_WRITE_NODE_H__
 
 #include <cdk/ast/expression_node.h>
 
 namespace og {
 
   /**
-   * Class for describing print nodes.
+   * Class for describing write nodes.
    */
-  class print_node: public cdk::basic_node {
+  class write_node: public cdk::basic_node {
     cdk::expression_node *_argument;
     bool _newline = false;
 
   public:
-    inline print_node(int lineno, cdk::expression_node *argument, bool newline = false) :
+    inline write_node(int lineno, cdk::expression_node *argument, bool newline = false) :
         cdk::basic_node(lineno), _argument(argument), _newline(newline) {
     }
 
@@ -26,7 +26,7 @@ namespace og {
     }
 
     void accept(basic_ast_visitor *sp, int level) {
-      sp->do_print_node(this, level);
+      sp->do_write_node(this, level);
     }
 
   };
