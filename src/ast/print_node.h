5255f64f5514fcd4bd16f5b4a78d85ca19522c4d
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-14 19:28:05 +0000

    ast/print_node: Add support for writeln

--- src/ast/print_node.h
+++ src/ast/print_node.h
@@ -10,15 +10,19 @@ namespace og {
    */
   class print_node: public cdk::basic_node {
     cdk::expression_node *_argument;
+    bool _newline = false;
 
   public:
-    inline print_node(int lineno, cdk::expression_node *argument) :
-        cdk::basic_node(lineno), _argument(argument) {
+    inline print_node(int lineno, cdk::expression_node *argument, bool newline = false) :
+        cdk::basic_node(lineno), _argument(argument, _newline(newline)) {
     }
 
   public:
     inline cdk::expression_node *argument() {
       return _argument;
+    }
+    bool newline() {
+      return _newline;
     }
 
     void accept(basic_ast_visitor *sp, int level) {
