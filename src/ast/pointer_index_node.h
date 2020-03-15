9f7d02d90ab49a3641a4efbed5e59a820f544b8b
João David <joaodavid1999@hotmail.com>
2020-03-15 18:33:30 +0000

    Add pointer_index_node

---
+++ src/ast/pointer_index_node.h
@@ -0,0 +1,35 @@
+#ifndef __OG_AST_POINTER_INDEX_H_
+#define __OG_AST_POINTER_INDEX_H_
+
+#include <cdk/ast/lvalue_node.h>
+#include "targets/basic_ast_visitor.h"
+
+namespace og {
+
+  class pointer_index_node: public cdk::lvalue_node {
+    cdk::expression_node *_base;
+    cdk::expression_node *_index;
+
+  public:
+    pointer_index_node(int lineno, cdk::expression_node *base, cdk::expression_node *index) :
+        cdk::lvalue_node(lineno), _base(base), _index(index) {
+    }
+
+  public:
+    cdk::expression_node *base() {
+      return _base;
+    }
+    cdk::expression_node *index() {
+      return _index;
+    }
+
+  public:
+    void accept(basic_ast_visitor *sp, int level) {
+      sp->do_pointer_index_node(this, level);
+    }
+
+  };
+
+} // og
+
+#endif
