f0d1b50095fbab8c4e2a6ca3e040e4df58477900
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-15 20:07:55 +0000

    Change pointer_index_node base to be a lvalue

--- src/ast/pointer_index_node.h
+++ src/ast/pointer_index_node.h
@@ -1,22 +1,23 @@
 #ifndef __OG_AST_POINTER_INDEX_H_
 #define __OG_AST_POINTER_INDEX_H_
 
+#include <cdk/ast/expression_node.h>
 #include <cdk/ast/lvalue_node.h>
 #include "targets/basic_ast_visitor.h"
 
 namespace og {
 
   class pointer_index_node: public cdk::lvalue_node {
-    cdk::expression_node *_base;
+    cdk::lvalue_node *_base;
     cdk::expression_node *_index;
 
   public:
-    pointer_index_node(int lineno, cdk::expression_node *base, cdk::expression_node *index) :
+    pointer_index_node(int lineno, cdk::lvalue_node *base, cdk::expression_node *index) :
         cdk::lvalue_node(lineno), _base(base), _index(index) {
     }
 
   public:
-    cdk::expression_node *base() {
+    cdk::lvalue_node *base() {
       return _base;
     }
     cdk::expression_node *index() {
