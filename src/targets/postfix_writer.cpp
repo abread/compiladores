ba9176883b93f50ca1a8d3a57f115085be6df705
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-15 17:47:17 +0000

    Add tuple_node (#25)

--- src/targets/postfix_writer.cpp
+++ src/targets/postfix_writer.cpp
@@ -347,3 +347,7 @@ void og::postfix_writer::do_if_else_node(og::if_else_node * const node, int lvl)
   node->elseblock()->accept(this, lvl + 2);
   _pf.LABEL(mklbl(lbl1 = lbl2));
 }
+
+void og::postfix_writer::do_tuple_node(og::tuple_node *const node, int lvl) {
+  // TODO
+}
--- src/targets/xml_writer.cpp
+++ src/targets/xml_writer.cpp
@@ -260,3 +260,7 @@ void og::xml_writer::do_if_else_node(og::if_else_node * const node, int lvl) {
   closeTag("else", lvl + 2);
   closeTag(node, lvl);
 }
+
+void og::xml_writer::do_tuple_node(og::tuple_node *const node, int lvl) {
+  // TODO
+}
