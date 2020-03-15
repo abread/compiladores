f3db7dd074ee5a410f68db10bb08ed556aba42d9
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-15 20:52:37 +0000

    Add tuple index node (#28)

---
+++ src/ast/tuple_index_node.h
@@ -0,0 +1,36 @@
+#ifndef __OG_AST_TUPLE_INDEX_H_
+#define __OG_AST_TUPLE_INDEX_H_
+
+#include <cdk/ast/lvalue_node.h>
+#include <cdk/ast/expression_node.h>
+#include "targets/basic_ast_visitor.h"
+
+namespace og {
+
+  class tuple_index_node: public cdk::lvalue_node {
+    cdk::lvalue_node *_base;
+    cdk::expression_node *_index;
+
+  public:
+    tuple_index_node(int lineno, cdk::lvalue_node *base, cdk::expression_node *index) :
+        cdk::lvalue_node(lineno), _base(base), _index(index) {
+    }
+
+  public:
+    cdk::lvalue_node *base() {
+      return _base;
+    }
+    cdk::expression_node *index() {
+      return _index;
+    }
+
+  public:
+    void accept(basic_ast_visitor *sp, int level) {
+      sp->do_tuple_index_node(this, level);
+    }
+
+  };
+
+} // og
+
+#endif
