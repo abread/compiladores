6ac2438f08db798cb02585da8dec495b50f60500
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-15 19:06:47 +0000

    stack_alloc_node: Change superclass to unary_operation_node

--- src/ast/stack_alloc_node.h
+++ src/ast/stack_alloc_node.h
@@ -2,23 +2,17 @@
 #define __OG_AST_STACK_ALLOC_H__
 
 #include <cdk/ast/expression_node.h>
+#include <cdk/ast/unary_operation_node.h>
 #include "targets/basic_ast_visitor.h"
 
 namespace og {
 
-  class stack_alloc_node: public cdk::expression_node {
-    cdk::expression_node *_argument;
+  class stack_alloc_node: public cdk::unary_operation_node {
   public:
     stack_alloc_node(int lineno, cdk::expression_node *argument) :
-        cdk::expression_node(lineno),  _argument(argument) {
-    }
+        cdk::unary_operation_node(lineno, argument) {}
 
   public:
-
-    cdk::expression_node *argument() {
-      return _argument;
-    }
-
     void accept(basic_ast_visitor *sp, int level) {
       sp->do_stack_alloc_node(this, level);
     }
