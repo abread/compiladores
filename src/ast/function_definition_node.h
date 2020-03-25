833235ea1f000bfb040966aa287af763b263951e
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-25 17:22:00 +0000

    ast: Refactor function_definition_node to be a typed_node

--- src/ast/function_definition_node.h
+++ src/ast/function_definition_node.h
@@ -2,10 +2,9 @@
 #define __OG_AST_FUNCTION_DEFINITION_H__
 
 #include <string>
+#include <memory>
 #include <cdk/ast/basic_node.h>
 #include <cdk/ast/sequence_node.h>
-#include <cdk/types/basic_type.h>
-#include <cdk/types/primitive_type.h>
 #include "ast/block_node.h"
 
 namespace og {
@@ -13,36 +12,27 @@ namespace og {
   /**
    * Class for describing function definitions.
    */
-  class function_definition_node: public cdk::basic_node {
+  class function_definition_node: public cdk::typed_node {
     int _qualifier;
-    cdk::basic_type *_type;
     std::string _identifier;
     cdk::sequence_node *_arguments;
-    og::block_node *_block;
+    block_node *_block;
 
   public:
-    function_definition_node(int lineno, int qualifier, const std::string &identifier, cdk::sequence_node *arguments, og::block_node *block) :
-        cdk::basic_node(lineno),
+    function_definition_node(int lineno, int qualifier, const std::string &identifier, cdk::sequence_node *arguments, block_node *block) :
+        function_definition_node(lineno, qualifier, std::make_shared<cdk::primitive_type>(0, cdk::typename_type::TYPE_VOID), identifier, arguments, block) {}
+    function_definition_node(int lineno, int qualifier, std::shared_ptr<cdk::basic_type> type, const std::string &identifier, cdk::sequence_node *arguments, block_node *block) :
+        cdk::typed_node(lineno),
         _qualifier(qualifier),
-        _type(new cdk::primitive_type(0, cdk::typename_type::TYPE_VOID)),
         _identifier(identifier),
         _arguments(arguments),
-        _block(block) {}
-
-    function_definition_node(int lineno, int qualifier, cdk::basic_type *type, const std::string &identifier, cdk::sequence_node *arguments, og::block_node *block) :
-        cdk::basic_node(lineno),
-        _qualifier(qualifier),
-        _type(type),
-        _identifier(identifier),
-        _arguments(arguments),
-        _block(block) {}
+        _block(block) {
+          this->type(type);
+        }
 
   public:
     int qualifier() {
       return _qualifier;
-    }
-    cdk::basic_type *type() {
-      return _type;
     }
     const std::string &identifier() const {
       return _identifier;
@@ -50,7 +40,7 @@ namespace og {
     cdk::sequence_node *arguments() {
       return _arguments;
     }
-    og::block_node *block() {
+    block_node *block() {
       return _block;
     }
 
