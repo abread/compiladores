eef8504eaf7cc3230cdeb607a1cb54bdd572b7bf
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-03-25 16:52:53 +0000

    ast/tuple_index_node: Simplify index handling

--- src/ast/tuple_index_node.h
+++ src/ast/tuple_index_node.h
@@ -8,10 +8,10 @@ namespace og {
 
   class tuple_index_node: public cdk::lvalue_node {
     cdk::expression_node *_base;
-    cdk::integer_node *_index;
+    int _index;
 
   public:
-    tuple_index_node(int lineno, cdk::expression_node *base, cdk::integer_node *index) :
+    tuple_index_node(int lineno, cdk::expression_node *base, int index) :
         cdk::lvalue_node(lineno), _base(base), _index(index) {
     }
 
@@ -19,7 +19,7 @@ namespace og {
     cdk::expression_node *base() {
       return _base;
     }
-    cdk::expression_node *index() {
+    int index() {
       return _index;
     }
 
