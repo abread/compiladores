1233abef1d824d506dedbaf8d400024c8fb5e91a
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-04-05 19:03:10 +0100

    Tweak AST constructors

--- src/ast/tuple_node.h
+++ src/ast/tuple_node.h
@@ -10,20 +10,19 @@ namespace og {
    * Class representing a tuple.
    */
   class tuple_node: public cdk::expression_node {
-    cdk::sequence_node *_elements;
+    cdk::sequence_node _elements;
 
   public:
     tuple_node(int lineno) :
-      cdk::expression_node(lineno), _elements(new cdk::sequence_node(lineno)) {}
+      cdk::expression_node(lineno), _elements(lineno) {}
 
-    tuple_node(int lineno, cdk::sequence_node *elements) :
-      cdk::expression_node(lineno), _elements(elements) {}
+    tuple_node(int lineno, cdk::expression_node* expr) :
+      cdk::expression_node(lineno), _elements(lineno, expr) {}
 
-    ~tuple_node() {
-      delete _elements;
-    }
+    tuple_node(int lineno, cdk::expression_node* expr, tuple_node* prev) :
+      cdk::expression_node(lineno), _elements(lineno, expr, &prev->elements()) {}
 
-    cdk::sequence_node *elements() {
+    cdk::sequence_node& elements() {
       return _elements;
     }
 
