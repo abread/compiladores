9763baf7d28e5085e1cdfce5e40fd943b4174a5f
João David <joaodavid1999@hotmail.com>
2020-04-06 10:28:18 +0100

    xml_writer: Implement binary operations

--- src/targets/xml_writer.cpp
+++ src/targets/xml_writer.cpp
@@ -12,13 +12,16 @@ void og::xml_writer::do_data_node(cdk::data_node * const node, int lvl) {
   // EMPTY
 }
 void og::xml_writer::do_double_node(cdk::double_node * const node, int lvl) {
+  process_literal(node, lvl);
+}
+void og::xml_writer::do_not_node(cdk::not_node * const node, int lvl) {
   // EMPTY
 }
 void og::xml_writer::do_and_node(cdk::and_node * const node, int lvl) {
-  // EMPTY
+  do_binary_operation(node, lvl);
 }
 void og::xml_writer::do_or_node(cdk::or_node * const node, int lvl) {
-  // EMPTY
+  do_binary_operation(node, lvl);
 }
 
 //---------------------------------------------------------------------------
