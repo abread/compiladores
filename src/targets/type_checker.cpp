f3b1a09c633e1840f0cb11fd596314f121cb5e1f
André Breda <andrebreda@tecnico.ulisboa.pt>
2020-05-16 22:55:35 +0100

    typechecker: Implement tuple indexing

--- src/targets/type_checker.cpp
+++ src/targets/type_checker.cpp
@@ -637,11 +637,22 @@ void og::type_checker::do_tuple_index_node(og::tuple_index_node *const node, int
   ASSERT_UNSPEC;
   node->base()->accept(this, lvl + 2);
 
-  if (! node->base()->is_typed(cdk::TYPE_STRUCT))
+  if (! node->base()->is_typed(cdk::TYPE_STRUCT)) {
     throw std::string("tuple index base is not a tuple");
+  }
 
-  // TODO
-  node->type(cdk::make_primitive_type(0, cdk::TYPE_UNSPEC));
+  auto baseType = cdk::structured_type_cast(node->base()->type());
+
+  if (node->index() < 1) {
+    throw std::string("invalid tuple index. remember that it starts with 1");
+  }
+
+  if (node->index() > baseType->length()) {
+    throw std::string("tuple index too big");
+  }
+
+  size_t idx = node->index() - 1;
+  node->type(baseType->component(idx));
 }
 
 void og::type_checker::do_sizeof_node(og::sizeof_node *const node, int lvl) {
